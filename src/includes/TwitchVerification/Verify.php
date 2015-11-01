7f520550270361ac9a3c5dfca08f5a8924afae65
Mitchfizz05 <me@mitchfizz05.net>
2015-11-01 19:10:23 +1030

    Add check to see if user is already in database

--- src/includes/TwitchVerification/Verify.php
+++ src/includes/TwitchVerification/Verify.php
@@ -10,6 +10,15 @@ class Verify {
         $database = new \PDO('mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('MYSQL_DATABASE'),
             getenv('MYSQL_USER'), getenv('MYSQL_PASS'));
 
+        // Check if user is in database already
+        $query = $database->prepare('SELECT id FROM users WHERE username=:username');
+        $query->bindValue(':username', $username);
+        $query->execute();
+        if ($query->rowCount() > 0) {
+            // Already in database
+            return;
+        }
+
         // Insert user into the database
         $query = $database->prepare('INSERT INTO users (username, added, ip) VALUES (:username, NOW(), :ip)');
         $query->bindValue(':username', $username);
